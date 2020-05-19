4fc5198c31a6e08b74e82ee02f12b8e37ebc9080
Daniel Burkard <burkard@b-linked.de>
2020-05-19 17:52:13 +0200

    Initial Commit

---
+++ BackgroundTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BackgroundTest
+//
+//  Created by Daniel Burkard on 19.05.20.
+//  Copyright © 2020 Daniel Burkard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
